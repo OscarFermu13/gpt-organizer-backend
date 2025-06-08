3a131f13f2c408b4f5e1c6498b6cc738cabdc77e
Oscar Fernandez <oscarferm13@gmail.com>
2025-06-08 13:53:26 +0200

    Delete chats on cascade when a folder is deleted

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -2,10 +2,12 @@ import { PrismaClient } from '@prisma/client';
 
 const globalForPrisma = globalThis;
 
-export const prisma =
+const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
     log: ['error'], // opcional
   });
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+
+module.exports = prisma;
