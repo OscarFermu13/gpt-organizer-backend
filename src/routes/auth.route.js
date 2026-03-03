0f9ed09ef636b59f2df4d077985efb461c12ca6a
Oscar Fernandez <32201800+OscarFermu13@users.noreply.github.com>
2026-03-03 15:38:36 +0100

    Update auth.route.js

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const router = express.Router()
+const auth = require('../middleware/authMiddleware')
 const { register, login, logout, validateUser, changePassword, deleteUser } = require('../controllers/auth.controller')
 
 router.post('/register', register)
 router.post('/login', login)
 router.post('/logout', logout)
-router.put('/change-password', changePassword)
-router.delete('/delete-user', deleteUser)
+router.put('/change-password', auth, changePassword)
+router.delete('/delete-user', auth, deleteUser)
 router.get('/validate', validateUser)
 
 module.exports = router
